bfdd33d818c88c53eefcfc48ffee6e96d46b4533
nate-kirschner <77256794+nate-kirschner@users.noreply.github.com>
2024-04-07 17:14:44 -0400

    updating cors

--- App.js
+++ App.js
@@ -10,10 +10,13 @@ import session from "express-session";
 import "dotenv/config";
 import AssignmentsRoutes from "./Kanbas/assignments/routes.js";
 
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+const CONNECTION_STRING =
+  process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 
-app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL }));
+app.use(cors({ credentials: true, origin: [...process.env.FRONTEND_URL] }));
 
 const sessionOptions = {
   secret: process.env.SESSION_SECRET,

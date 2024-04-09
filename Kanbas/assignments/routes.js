5fa48cea143d7e5390c7bb8ee737612684cddc8f
nate-kirschner <77256794+nate-kirschner@users.noreply.github.com>
2024-04-09 16:19:41 -0400

    update assignments

--- Kanbas/assignments/routes.js
+++ Kanbas/assignments/routes.js
@@ -2,8 +2,8 @@ import * as dao from "./dao.js";
 
 export default function AssignmentsRoutes(app) {
   app.get("/api/assignments/:cid", async (req, res) => {
-    const { id } = req.params;
-    const course = await dao.findAssignmentsById(id);
+    const { cid } = req.params;
+    const course = await dao.findAssignmentsById(cid);
     if (!course) {
       res.status(404).send("Course not found");
       return;
